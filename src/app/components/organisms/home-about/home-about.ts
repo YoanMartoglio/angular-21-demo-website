ec6b1d4fa108584f1ba8538647da37d74ff2af7b
YoanMartoglio <yoan.martoglio@gmail.com>
2025-12-11 09:27:34 +0100

    feat(home-about): add HomeAbout component

---
+++ src/app/components/organisms/home-about/home-about.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-about',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-about.html',
+  styleUrl: './home-about.scss',
+})
+export class HomeAbout {}
--- src/app/pages/accueil/accueil.ts
+++ src/app/pages/accueil/accueil.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeHero } from '../../components/organisms/home-hero/home-hero';
+import { HomeAbout } from '../../components/organisms/home-about/home-about';
 
 @Component({
   selector: 'app-accueil',
-  imports: [HomeHero],
+  standalone: true,
+  imports: [HomeHero, HomeAbout],
   templateUrl: './accueil.html',
   styleUrl: './accueil.scss',
 })
