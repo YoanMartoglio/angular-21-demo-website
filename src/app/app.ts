1af3c0b45068037aaefebe790c5983a6a96184c0
YoanMartoglio <yoan.martoglio@gmail.com>
2025-12-18 13:39:45 +0100

    refactor(home-works): enhance layout and responsiveness, update project details

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './components/organisms/header/header';
 import { Footer } from './components/organisms/footer/footer';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { 
-  faHtml5, 
-  faCss3Alt, 
-  faJs, 
+import {
+  faHtml5,
+  faCss3Alt,
+  faJs,
   faAngular,
   faGithub,
-  faLinkedin
+  faLinkedin,
 } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
@@ -21,18 +21,11 @@ import {
 })
 export class App implements OnInit {
   private iconLibrary = inject(FaIconLibrary);
-  
+
   protected readonly title = signal('angular-21-demo-website');
 
   ngOnInit() {
     // Register required icons
-    this.iconLibrary.addIcons(
-      faHtml5,
-      faCss3Alt,
-      faJs,
-      faAngular,
-      faGithub,
-      faLinkedin
-    );
+    this.iconLibrary.addIcons(faHtml5, faCss3Alt, faJs, faAngular, faGithub, faLinkedin);
   }
 }
--- src/app/components/organisms/home-works/home-works.ts
+++ src/app/components/organisms/home-works/home-works.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { map } from 'rxjs/operators';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 interface WorksExamples {
   projectId: number;
@@ -16,17 +19,16 @@ interface WorksExamples {
   textColor: string;
   class: string;
   ariaLabel: string;
-  cols: number;
-  rows: number;
+  tileKey: string;
 }
 
 @Component({
   selector: 'app-home-works',
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, AsyncPipe],
   templateUrl: './home-works.html',
   styleUrl: './home-works.scss',
 })
-export class HomeWorks implements OnInit {
+export class HomeWorks {
   /**
    * Convertit une couleur hexadécimale en valeurs RGB
    * @param hex - Couleur hexadécimale (ex: #E26352)
@@ -57,8 +59,7 @@ export class HomeWorks implements OnInit {
       textColor: 'white',
       class: 'cma-cgm',
       ariaLabel: 'CMA CGM - Site e-commerce',
-      cols: 5,
-      rows: 1,
+      tileKey: 'tiles8',
     },
     {
       projectId: 2,
@@ -68,14 +69,13 @@ export class HomeWorks implements OnInit {
       logo: 'assets/images/works/cma-cgm/cma-cgm-logo.svg',
       description: "Conception d'une interface d'IA pour la CMA CGM",
       technologies: ['Figma', 'FigJam', 'Design thinking'],
-      image: 'assets/images/works/cma-cgm/cma-cgm-corp.jpg',
+      image: 'assets/images/works/cma-cgm/cma-cgm-corp-2.jpg',
       link: 'https://www.project2.com',
       color: '#1993D0',
       textColor: 'white',
       class: 'cma-cgm-alt',
       ariaLabel: "CMA CGM - Interface d'IA",
-      cols: 3,
-      rows: 1,
+      tileKey: 'tiles4',
     },
     {
       projectId: 3,
@@ -85,14 +85,13 @@ export class HomeWorks implements OnInit {
       logo: 'assets/images/works/adnov/adnov-logo.jpg',
       description: 'Maquettes des parcours utilisateurs, et intégration web',
       technologies: ['Figma', 'Design thinking', 'Angular', 'HTML5', 'CSS3', 'Javascript'],
-      image: 'assets/images/works/cma-cgm/cma-cgm-corp.jpg',
+      image: 'assets/images/works/adnov/adnov-corp-1.webp',
       link: 'https://www.project2.com',
-      color: '#1993D0',
+      color: '#003C5A',
       textColor: 'white',
-      class: 'cma-cgm-alt',
-      ariaLabel: 'CMA CGM - Immobilier',
-      cols: 4,
-      rows: 1,
+      class: 'adnov',
+      ariaLabel: 'ADNOV - Immobilier',
+      tileKey: 'tiles6',
     },
     {
       projectId: 4,
@@ -102,37 +101,141 @@ export class HomeWorks implements OnInit {
       logo: 'assets/images/works/adnov/adnov-logo.jpg',
       description: 'Maquettes des parcours utilisateurs, et intégration web',
       technologies: ['Figma', 'Design thinking', 'Angular', 'HTML5', 'CSS3', 'Javascript'],
-      image: 'assets/images/works/cma-cgm/cma-cgm-corp.jpg',
+      image: 'assets/images/works/adnov/adnov-corp-2.webp',
       link: 'https://www.project2.com',
-      color: '#1993D0',
+      color: '#FECF49',
+      textColor: 'black',
+      class: 'adnov-alt',
+      ariaLabel: 'ADNOV - Perval',
+      tileKey: 'tiles6',
+    },
+    {
+      projectId: 5,
+      title: 'Fleetenergies',
+      subtitle: 'Application web de gestion logistique des transports',
+      job: 'Lead UX UI designer & LeadDéveloppeur front-end',
+      logo: 'assets/images/works/fleetenergies/fleetenergies-logo.png',
+      description:
+        "Maquettes des parcours utilisateurs, refonte de l'identité visuelle de la maque et intégration web",
+      technologies: ['HTML5', 'CSS3', 'Javascript', 'AdobeXD', 'Design thinking'],
+      image: 'assets/images/works/fleetenergies/fleetenergies-corp-1.png',
+      link: 'https://www.project2.com',
+      color: '#409D44',
       textColor: 'white',
-      class: 'cma-cgm-alt',
-      ariaLabel: 'CMA CGM - Perval',
-      cols: 4,
-      rows: 1,
+      class: 'fleetenergies',
+      ariaLabel: 'Fleetenergies - Application web de gestion logistique des transports',
+      tileKey: 'tiles5',
+    },
+    {
+      projectId: 5,
+      title: 'Externa',
+      subtitle: 'Sites web de solutions de gestion RH',
+      job: 'Lead UX UI designer & Lead Développeur front-end',
+      logo: 'assets/images/works/externa/externa-logo.png',
+      description: 'Refonte des sites Wordpress et refactorisation vers Angular 21',
+      technologies: [
+        'Angular',
+        'HTML5',
+        'CSS3',
+        'Javascript',
+        'Wordpress',
+        'Figma',
+        'Design thinking',
+      ],
+      image: 'assets/images/works/externa/externa-corp-1.jpg',
+      link: 'https://www.project2.com',
+      color: '#c8a778',
+      textColor: 'black',
+      class: 'externa',
+      ariaLabel: 'Externa - Sites web de solutions de gestion RH',
+      tileKey: 'tiles7',
+    },
+    {
+      projectId: 6,
+      title: "Château de l'Engarran",
+      subtitle: 'Site e-commerce et site vitrine',
+      job: 'Lead développeur front-end',
+      logo: 'assets/images/works/engarran/engarran-logo.svg',
+      description: 'Intégration web',
+      technologies: ['HTML5', 'CSS3', 'Javascript', 'Presatshop', 'Wordpress'],
+      image: 'assets/images/works/engarran/engarran-corp-1.jpg',
+      link: 'https://www.project2.com',
+      color: '#C1A050',
+      textColor: 'white',
+      class: 'engarran',
+      ariaLabel: "Château de l'Engarran - Site e-commerce et site vitrine",
+      tileKey: 'tiles6',
+    },
+    {
+      projectId: 7,
+      title: 'apptamin',
+      subtitle: "Site web de l'app video agency",
+      job: 'Lead développeur front-end',
+      logo: 'assets/images/works/apptamin/apptamin-logo.png',
+      description: 'Intégration web et refonte du site Wordpress',
+      technologies: ['HTML5', 'CSS3', 'Javascript', 'Wordpress'],
+      image: 'assets/images/works/apptamin/apptamin-corp-1.jpg',
+      link: 'https://www.project2.com',
+      color: '#003C5A',
+      textColor: 'white',
+      class: 'apptamin',
+      ariaLabel: "apptamin - Site web de l'app video agency",
+      tileKey: 'tiles6',
     },
   ];
 
-  private breakpointObserver = inject(BreakpointObserver);
-  cols: number = 8;
+  worksGridLayout!: Observable<any>;
 
-  ngOnInit() {
-    this.breakpointObserver
-      .observe([
-        Breakpoints.HandsetPortrait,
-        Breakpoints.HandsetLandscape,
-        Breakpoints.Tablet,
-        Breakpoints.Web,
-      ])
-      .subscribe((result) => {
-        const breakpoints = result.breakpoints;
-        if (breakpoints[Breakpoints.HandsetPortrait] || breakpoints[Breakpoints.HandsetLandscape]) {
-          this.cols = 2;
-          console.log('Mobile detected, cols:', this.cols); // Debug
-        } else {
-          this.cols = 8;
-          console.log('Desktop detected, cols:', this.cols); // Debug
-        }
-      });
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.worksGridLayout = this.breakpointObserver
+      .observe([Breakpoints.Handset, Breakpoints.Tablet])
+      .pipe(
+        map(({ matches }) => {
+          if (matches) {
+            return {
+              columns: 1,
+              gutters: '16px',
+              tiles1: { cols: 1, rows: 1 },
+              tiles2: { cols: 1, rows: 1 },
+              tiles3: { cols: 1, rows: 1 },
+              tiles4: { cols: 1, rows: 1 },
+              tiles5: { cols: 1, rows: 1 },
+              tiles6: { cols: 1, rows: 1 },
+              tiles7: { cols: 1, rows: 1 },
+              tiles8: { cols: 1, rows: 1 },
+              tiles9: { cols: 1, rows: 1 },
+              tiles10: { cols: 1, rows: 1 },
+              tiles11: { cols: 1, rows: 1 },
+              tiles12: { cols: 1, rows: 1 },
+            };
+          }
+
+          return {
+            columns: 12,
+            gutters: '8px',
+            tiles1: { cols: 1, rows: 1 },
+            tiles2: { cols: 2, rows: 1 },
+            tiles3: { cols: 3, rows: 1 },
+            tiles4: { cols: 4, rows: 1 },
+            tiles5: { cols: 5, rows: 1 },
+            tiles6: { cols: 6, rows: 1 },
+            tiles7: { cols: 7, rows: 1 },
+            tiles8: { cols: 8, rows: 1 },
+            tiles9: { cols: 9, rows: 1 },
+            tiles10: { cols: 10, rows: 1 },
+            tiles11: { cols: 11, rows: 1 },
+            tiles12: { cols: 12, rows: 1 },
+          };
+        })
+      );
+  }
+
+  getTileLayout(layout: any, index: number): { cols: number; rows: number } {
+    const tileKey = `tiles${index + 1}` as keyof typeof layout;
+    return layout[tileKey] || { cols: 1, rows: 1 };
+  }
+
+  getTileDimensions(layout: any, tileKey: string): { cols: number; rows: number } {
+    return layout[tileKey] || { cols: 1, rows: 1 };
   }
 }
