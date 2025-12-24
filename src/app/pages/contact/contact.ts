5db981a756f1e30de98e18a60626731c4e5cc88b
YoanMartoglio <yoan.martoglio@gmail.com>
2025-12-24 09:36:17 +0100

    feat(contact, expertises): redesign contact and expertises pages with new layouts and styles for improved user experience

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { FormContact } from '../../components/molecules/form-contact/form-contact';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormContact],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
-export class Contact {
-
-}
+export class Contact {}
